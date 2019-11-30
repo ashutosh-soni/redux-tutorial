678e8b1f9d785d9cca9709926f3ed4e60035edac
Ashutosh Soni <viknat1994@gmail.com>
2019-11-30 22:16:06 +0530

    Simple Implementation of store, combineReducers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore, combineReducers} from "redux";
+
+const personReducer = (state={}, {type, payload}) =>{
+    if(type === "UPDATE_PERSON"){
+        return {name:payload};
+    }
+    return state;
+};
+
+const gameReducer = (state={}, {type, payload}) =>{
+    if(type === "UPDATE_GAME"){
+        return {name:payload};
+    }
+
+    return state;
+};
+
+const AllReducers = combineReducers({game:gameReducer, person:personReducer});
+const InitialStates = {
+    game: {name: "Football"},
+    person: {name: "Sarthak"}
+};
+
+
+const store= createStore(AllReducers, InitialStates);
+console.log(store.getState());
+
+store.dispatch({type:"UPDATE_PERSON", payload:"bitfumes"});
+console.log(store.getState());
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
